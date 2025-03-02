aa5cae4a4e0f806df8532df6ab90f688eb465e21
mojoex <mojoex@gmail.com>
2025-03-02 02:02:48 +0000

    Init - Further initial tweaks

---
+++ SHDDB.Standalone/Theme/CustomTheme.cs
@@ -0,0 +1,34 @@
+﻿using MudBlazor;
+
+namespace SHDDB.Standalone.Theme
+{
+    public static class CustomTheme
+    {
+        public static MudTheme Theme = new()
+        {
+        };
+
+        public const string Exotic = "#ff6f36";
+        public const string ExoticDarken = "#fe5608";
+        public const string ExoticLighten = "#ff895c";
+
+        public const string Gearset = "#01ff90";
+        public const string GearsetDarken = "#00f55c";
+        public const string GearsetLighten = "#baffd2";
+
+        public const string Perfect = "#EAA213";
+        public const string PerfectLighten = "#ecba1f";
+        public const string PerfectDarken = "#e77a00";
+
+        public const string HighEnd = "#ffaf10";
+        public const string HighEndLighten = "#ffbd14";
+        public const string HighEndDarken = "#fe9d10";
+
+        public const string Secondary = "#70685c";
+
+        public const string RedCore = "#a43b3b";
+        public const string UtilityStat = "#3ba4a4";
+        public const string Condition = "#3ba43b";
+        public const string Stacking = "#a4a43b";
+    }
+}
