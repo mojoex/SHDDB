f5beb553cd656b271df50a0d347217d6c594eab6
mojoex <mojoex@gmail.com>
2025-03-10 23:24:38 +0000

    Progress tracker and Changelog complete

--- SHDDB.DTO/Configuration/Changelog.cs
+++ SHDDB.DTO/Configuration/Changelog.cs
@@ -2,41 +2,19 @@
 
 namespace SHDDB.DTO.Configuration
 {
-    public class Changelog
-    {
-        [JsonPropertyName("version")]
-        public SemVer Version { get; set; }
-
-        [JsonPropertyName("titleUpdate")]
-        public string TitleUpdate { get; set; }
-
-        [JsonPropertyName("releases")]
-        public Dictionary<string, Release> Releases { get; set; }
-    }
-
-    public class SemVer
-    {
-        [JsonPropertyName("major")]
-        public int Major { get; set; }
-
-        [JsonPropertyName("minor")]
-        public int Minor { get; set; }
-
-        [JsonPropertyName("patch")]
-        public int Patch { get; set; }
-
-        [JsonPropertyName("tag")]
-        public string? Tag { get; set; }
-
-        public string AsString() => $"{Major}.{Minor}.{Patch}{(Tag != null ? $"-{Tag}" : "")}";
-    }
-
     public class Release
     {
-        [JsonPropertyName("date")]
-        public string Date { get; set; }
+        [JsonIgnore]
+        public string Version { get; set; } = "v0.0.1";
 
-        [JsonPropertyName("changelog")]
-        public List<string> Changelog { get; set; }
+        [JsonPropertyName("titleUpdate")]
+        public string TitleUpdate { get; set; } = string.Empty;
+
+        public DateTime Date { get; set; }
+
+        [JsonIgnore]
+        public List<string> Notes { get; set; } = [];
+
+        public string Uri { get; set; } = string.Empty;
     }
 }
